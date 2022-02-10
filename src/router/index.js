edfe04159789aa8c3502ff18da21b62869eef388
Mindaugas <48929117+mindaugas-pro@users.noreply.github.com>
2022-02-10 12:32:22 +0200

    new production version created

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,7 @@ const routes = [
 ];
 
 const router = createRouter({
-	history: createWebHistory('/list-with-products/'),
+	history: createWebHistory(),
 	routes,
 });
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: '/list-with-products/'
+    publicPath: '/demo-list-with-products/'
 }
